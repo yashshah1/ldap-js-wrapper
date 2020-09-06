a999f70ba605247046d76e358ec3ba84a078dcd1
Yash Shah <yashah1234@gmail.com>
2020-09-06 18:46:49 +0530

    refactored index.js * incorporated error messages * added connectTimeoutError and timeoutError

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const ldap = require("ldapjs");
 const defaultDataCheck = require("./utils");
+const {
+  INCONSISTENT_INPUT,
+  TIMEOUT_ERROR,
+  CONNECT_TIMEOUT_ERROR,
+  SEARCH_ERROR,
+  INVALID_CREDENTIALS,
+  UNEXPECTED_ERROR,
+} = require("./constant");
 
 const authenticatePromise = (username, password, options = {}) => {
   return new Promise((resolve, reject) => {
@@ -9,36 +17,51 @@ const authenticatePromise = (username, password, options = {}) => {
       returnData: false,
       baseSearchString: "dc=coep,dc=org,dc=in",
       dataCheck: "coep",
+      timeout: 1000,
+      connectTimeout: 1000,
       ...options,
     };
 
     if (options.dataCheck === "coep") {
-      if (!defaultDataCheck(username, password)) return Promise.reject();
+      if (!defaultDataCheck(username, password)) {
+        return Promise.reject(INCONSISTENT_INPUT);
+      }
     } else if (
       options.dataCheck !== null &&
       typeof options.dataCheck === "function"
     ) {
-      if (!options.dataCheck(username, password)) return Promise.reject();
+      if (!options.dataCheck(username, password)) {
+        return Promise.reject(INCONSISTENT_INPUT);
+      }
     }
 
     const client = ldap.createClient({
       url: options.url,
+      timeout: options.timeout,
+      connectTimeout: options.connectTimeout,
     });
-
+    console.log(client);
     const opts = {
       filter: `cn=${username}`,
       scope: "sub",
       ...(options.returnData ? {} : { attributes: ["dn"] }),
     };
+    client
+      .on("timeout", () => {
+        reject(TIMEOUT_ERROR);
+      })
+      .on("connectTimeout", () => {
+        reject(CONNECT_TIMEOUT_ERROR);
+      });
 
     try {
       client.search(options.baseSearchString, opts, (err, ldapSearchRes) => {
-        if (err) reject();
+        if (err) reject(SEARCH_ERROR);
 
         ldapSearchRes.on("searchEntry", entry => {
           const { dn } = entry.object;
           client.bind(dn, password, err => {
-            if (err) reject(err);
+            if (err) reject(INVALID_CREDENTIALS);
             if (options.returnData) resolve(entry.object);
             resolve();
           });
@@ -48,7 +71,7 @@ const authenticatePromise = (username, password, options = {}) => {
         });
       });
     } catch (err) {
-      reject();
+      reject(UNEXPECTED_ERROR);
     }
   });
 };
